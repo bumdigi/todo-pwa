9e5a2a7b75bcefbbc8e2abd6ef18cf070b122f07
sangbum <bum922@gmail.com>
2020-01-07 15:27:58 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <div class="columns is-multiline">
+                <div>index 입니다.</div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+      
+    }
+</script>
